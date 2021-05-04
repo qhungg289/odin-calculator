192e38ea816d1f66db677fb95c680584911a4673
Hung Duong Quang <qhungg289@protonmail.com>
2021-05-04 21:47:17 +0700

    Add operate function

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ function subtract(a, b) {return a - b;}
 function multiply(a, b) {return a * b;}
 
 function divide(a, b) {return a / b;};
+
+function operate(a, b, operator) {
+    switch(operator) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
+}
